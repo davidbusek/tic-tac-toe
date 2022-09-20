d398f508d899a75b32b221d19aeef700859cdae5
sssvt-busek-david <busekdavid@sssvt.cz>
2022-09-20 07:18:44 +0200

    test

--- random/Program.cs
+++ random/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("watch out");
             Console.WriteLine("lets try it");
+            //test
           
         }
     }
